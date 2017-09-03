3c1f03751c82d95e6239c29d39acbd38fe72d937
Nick Kharaim <nick.kharaim@gmail.com>
2017-09-03 10:50:16 +0200

    Test script.js

--- index.js
+++ index.js
@@ -1,7 +1,24 @@
 module.exports = {
+    book: {
+        assets: './assets',
+        js: [
+            './select.js'
+        ],
+        // still not work!
+        html: {
+            'html:end': function() {
+                console.log('html:end')
+                return '<!-- End of book -->';
+            }
+        }
+    },
     hooks: {
-        "finish": function() {
-            console.log('select-version finish hook');
+        init: function() {
+            console.log( 'init!' );
+        },
+
+        finish: function() {
+            console.log( 'finish!' );
         }
     }
 };
